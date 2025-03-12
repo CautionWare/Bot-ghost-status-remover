840bdc713860ce3f05d9e778e3dede3815deb395
CautionWare <randomacc42565@gmail.com>
2025-03-12 10:53:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["Automation | BrilliantMachines"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'dnd',];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
